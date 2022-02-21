b8dc9043a98cf177a2ceba4626784bd0aaa8d66a
Bogger-byte <nik.troshnev@gmail.com>
2022-02-21 23:19:24 +0700

    Switched from UploadPngFile to UploadMcaFile schema

--- app/schemas/files.py
+++ app/schemas/files.py
@@ -1,23 +1,21 @@
-__all__ = ["UploadPngFile"]
+__all__ = ["UploadMcaFile"]
 
 import os
 from typing import Type, Any, Iterable, Callable
 
-from fastapi import UploadFile
+from starlette.datastructures import UploadFile as StarletteUploadFile
 
 
-class UploadPngFile(UploadFile):
+class UploadMcaFile(StarletteUploadFile):
     @classmethod
-    def __get_validators__(cls: Type["UploadPngFile"]) -> Iterable[Callable[..., Any]]:
+    def __get_validators__(cls: Type["UploadMcaFile"]) -> Iterable[Callable[..., Any]]:
         yield cls.validate
 
     @classmethod
-    def validate(cls: Type["UploadPngFile"], v: Any) -> Any:
-        if not isinstance(v, UploadFile):
+    def validate(cls: Type["UploadMcaFile"], v: Any) -> Any:
+        if not isinstance(v, StarletteUploadFile):
             raise ValueError(f"Expected UploadFile, received: {type(v)}")
-        # if v.content_type != "image/png":
-        #     raise ValueError("Expected png content type")
-        # _, extension = os.path.splitext(v.filename.lower())
-        # if extension != ".png":
-        #     raise ValueError("Expected png extension")
+        _, extension = os.path.splitext(v.filename.lower())
+        if extension != ".mca":
+            raise ValueError(f"Expected .mca file extension, recieved: {extension}")
         return v
