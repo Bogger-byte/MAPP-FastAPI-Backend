f569ec846ff64c336dc492b181a7ca0cdde8fe13
Bogger-byte <nik.troshnev@gmail.com>
2022-02-21 23:27:16 +0700

    Separated "users.py" models to different modules

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-__all__ = ["User", "Server"]
+__all__ = ["User"]
 
 import uuid
 
@@ -17,13 +17,4 @@ class User(Model):
     is_super = fields.BooleanField(default=False)
 
 
-class Server(Model):
-    id = fields.UUIDField(pk=True, default=uuid.uuid4())
-    owner = fields.ForeignKeyField("models.User", related_name="linked_servers")
-    name = fields.CharField(24, unique=True)
-    host = fields.CharField(24, unique=True)
-    api_key = fields.CharField(128)
-    email = fields.TextField()
-    created_at = fields.DatetimeField(auto_now_add=True)
-    modified_at = fields.DatetimeField(auto_now=True)
-    is_disabled = fields.BooleanField(default=False)
+
