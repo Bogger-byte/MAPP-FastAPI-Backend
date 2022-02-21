23ce5d625477d9635d80dfa1a47195edf934b35c
Bogger-byte <nik.troshnev@gmail.com>
2022-02-21 23:12:31 +0700

    Moved "server_snapshot.py" module to "servers_folder" package

---
+++ app/controllers/servers_folder/__init__.py
@@ -0,0 +1,22 @@
+__all__ = ["regions", "create_new", "get_regions_path"]
+
+import os
+from pathlib import Path
+from typing import Union
+
+from . import regions
+
+from settings import SERVER_REGIONS, SERVER_CACHE, SERVER_LOGS
+
+
+def create_new(server_uuid: str) -> None:
+    os.makedirs(f"{SERVER_REGIONS}/{server_uuid}")
+    os.makedirs(f"{SERVER_CACHE}/{server_uuid}")
+    os.makedirs(f"{SERVER_LOGS}/{server_uuid}")
+
+
+def get_regions_path(
+        server_uuid: str
+) -> Union[Path, None]:
+    folder_path = f"{SERVER_REGIONS}/{server_uuid}"
+    return folder_path if os.path.exists(folder_path) else None
