a7ca2151af0cfe39bfae01ccf48f361bdc48873c
Bogger-byte <nik.troshnev@gmail.com>
2022-02-21 23:17:42 +0700

    Implemented get_current_server and require_jwt_token dependency

--- app/controllers/authentication.py
+++ app/controllers/authentication.py
@@ -1,18 +1,35 @@
-__all__ = ["authorize_user", "get_current_user", "get_current_admin", "authorize_server"]
+__all__ = [
+    "oauth2_scheme",
+    "authorize_user",
+    "get_current_user",
+    "get_current_admin",
+    "authorize_server",
+    "get_current_server",
+    "require_jwt_token"
+]
 
-from datetime import datetime
-
-from fastapi import Depends, Query, Header
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
-
-from controllers import exceptions as exc
-from controllers.security import verify_password, decode_jwt
 
 import models
 import schemas
+from controllers import exceptions as exc
+from controllers.security import verify_password, JWTToken
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/auth/jwt/token",
+    scopes={scope.value: "" for scope in schemas.Scopes}
+)
+
+
+def require_jwt_token(
+        token: str = Depends(oauth2_scheme)
+) -> JWTToken:
+    try:
+        return JWTToken(token)
+    except ValueError:
+        raise exc.validation_exception
 
 
 async def authorize_user(
@@ -28,19 +45,11 @@ async def authorize_user(
 
 
 async def get_current_user(
-        token: str = Depends(oauth2_scheme)
+        token: JWTToken = Depends(require_jwt_token)
 ) -> schemas.User:
-    payload = decode_jwt(token)
-    if payload is None:
+    if token.is_expired:
         raise exc.credentials_exception
-    username = payload.get("sub")
-    expire = payload.get("exp")
-    if username is None or expire is None:
-        raise exc.credentials_exception
-
-    if datetime.utcfromtimestamp(expire) < datetime.utcnow():
-        raise exc.credentials_exception
-    user_obj = await models.User.get_or_none(username=username)
+    user_obj = await models.User.get_or_none(id=token.subject)
     if user_obj is None:
         raise exc.credentials_exception
     if user_obj.is_disabled:
@@ -49,7 +58,7 @@ async def get_current_user(
 
 
 async def get_current_admin(
-        token: str = Depends(oauth2_scheme)
+        token: JWTToken = Depends(require_jwt_token)
 ) -> schemas.User:
     user = await get_current_user(token)
     if not user.is_super:
@@ -58,25 +67,27 @@ async def get_current_admin(
 
 
 async def authorize_server(
-        host: str = Query(..., regex=schemas.ip_validation_regex),
-        api_key: str = Header(...)
+        client_id: str,
+        client_secret: str
 ) -> schemas.Server:
-    server_obj = await models.Server.get_or_none(host=host)
+    server_obj = await models.Server.get_or_none(id=client_id)
     if server_obj is None:
         raise exc.credentials_exception
-    if not verify_password(api_key, server_obj.api_key):
+    if not verify_password(client_secret, server_obj.api_key):
         raise exc.credentials_exception
     if server_obj.is_disabled:
         raise exc.object_was_deleted
     return await schemas.Server.from_tortoise_orm(server_obj)
 
 
-# user_obj = await models.User.get_or_none(username=user.username)
-# await user_obj.fetch_related("linked_servers")
-# linked_server_hosts = [server.host for server in list(user_obj.linked_servers)]
-# if host not in linked_server_hosts:
-#     raise exc.forbidden_act_exception
-#
-# server_obj = await models.Server.get_or_none(host=host)
-# if server_obj is None:
-#     raise exc.not_exist_exception
+async def get_current_server(
+        token: JWTToken = Depends(require_jwt_token)
+) -> schemas.Server:
+    if token.is_expired:
+        raise exc.credentials_exception
+    user_obj = await models.Server.get_or_none(id=token.subject)
+    if user_obj is None:
+        raise exc.credentials_exception
+    if user_obj.is_disabled:
+        raise exc.object_was_deleted
+    return await schemas.Server.from_tortoise_orm(user_obj)
