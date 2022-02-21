449f6436cdcf84f582f28f39caa64f868f2a6e96
Bogger-byte <nik.troshnev@gmail.com>
2022-02-21 23:16:22 +0700

    Implemented scopes authorization for servers and users API. Included "/refresh" route for JWT token refreshing

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,29 +1,43 @@
 __all__ = []
 
-from fastapi import APIRouter, Depends
-from starlette import status
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
 
-from datetime import timedelta
-
-from settings import ACCESS_TOKEN_EXPIRE_MINUTES
-from controllers.authentication import authorize_user
-from controllers.security import encode_jwt
-
+from controllers import exceptions as exc
+from controllers.authentication import authorize_user, authorize_server, require_jwt_token
+from controllers.security import JWTToken
+from schemas import Scopes, CredentialsValidationForm
 
 router = APIRouter()
 
 
-@router.post("/token")
+@router.post("/jwt/token")
 async def login_for_access_token(
-        credentials: OAuth2PasswordRequestForm = Depends()
+        credentials: CredentialsValidationForm = Depends(),
 ) -> JSONResponse:
-    user = await authorize_user(credentials.username, credentials.password)
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = encode_jwt(data={"sub": user.username}, expires_delta=access_token_expires)
+
+    subject = await authorize_user(credentials.username, credentials.password)
+    if Scopes.MINECRAFT_SERVER.value in credentials.scopes:
+        subject = await authorize_server(credentials.client_id, credentials.client_secret)
+
+    access_token = JWTToken.create_access_token(subject=str(subject.id))
+    refresh_token = JWTToken.create_refresh_token(subject=str(subject.id))
     return JSONResponse(
-        content={"access_token": access_token, "token_type": "bearer"},
+        content={"access_token": access_token, "refresh_token": refresh_token},
         status_code=status.HTTP_200_OK
     )
 
+
+@router.post("/jwt/refresh")
+async def refresh_access_token(
+        token: JWTToken = Depends(require_jwt_token)
+) -> JSONResponse:
+    if not token.is_refresh_token:
+        raise exc.validation_exception
+
+    access_token = JWTToken.create_access_token(subject=token.subject)
+    refresh_token = JWTToken.create_refresh_token(subject=token.subject)
+    return JSONResponse(
+        content={"access_token": access_token, "refresh_token": refresh_token},
+        status_code=status.HTTP_200_OK
+    )
