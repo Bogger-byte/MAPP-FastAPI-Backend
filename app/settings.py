c0464d46222961cb297ea0df7e700b5eea90f68f
Bogger-byte <nik.troshnev@gmail.com>
2022-02-21 23:26:51 +0700

    Created new JWT token settings

--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,14 @@
+from datetime import timedelta
+
 DATABASE_URL = "sqlite://./database/user.db"
 
 MODELS_FILE = "models"
 
-JWT_SECRET_KEY = "SECRET"
-JWT_ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SERVER_REGIONS = "servers/regions"
+SERVER_CACHE = "servers/cache"
+SERVER_LOGS = "servers/logs"
+
+JWT_SECRET = "SECRET"
+JWT_DECODE_ALGORYTHM = "HS256"
+JWT_ACCESS_TOKEN_EXPIRE = timedelta(minutes=30)
+JWT_REFRESH_TOKEN_EXPIRE = timedelta(days=15)
