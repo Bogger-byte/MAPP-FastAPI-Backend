04bce5c89734bc0cd57ff5b1ddeee62126b5db4e
Bogger-byte <nik.troshnev@gmail.com>
2022-02-21 23:23:00 +0700

    Implemented JWT token authorization for routes

--- app/routers/server.py
+++ app/routers/server.py
@@ -2,16 +2,16 @@ __all__ = []
 
 import os.path
 
-from fastapi import APIRouter, Query, Depends, Body, File, UploadFile
+from fastapi import APIRouter, Depends, Body, File, Query, UploadFile
 from starlette import status
 from fastapi.responses import JSONResponse, FileResponse
 
 import models
 import schemas
 
-from controllers.authentication import authorize_server
+from controllers.authentication import get_current_server
 from controllers import exceptions as exc
-from controllers import server_snapshot
+from controllers import servers_folder
 
 
 router = APIRouter()
@@ -24,58 +24,60 @@ async def get_server_info(
     pass
 
 
-@router.get("/{host}/check-credentials")
-async def get_server_info(
-        server: schemas.Server = Depends(authorize_server)
-) -> JSONResponse:
-    return JSONResponse(
-        content={
-            "id": str(server.id),
-            "name": server.name,
-            "email": server.email
-        },
-        status_code=status.HTTP_200_OK
-    )
-
-
 @router.post("/{host}/players-data")
 async def upload_players_data(
         players_data: schemas.PlayersData = Body(...),
-        current_server: schemas.Server = Depends(authorize_server)
+        current_server: schemas.Server = Depends(get_current_server)
 ) -> JSONResponse:
+    await models.ServerData(
+        id=current_server.id,
+        players_data=players_data.json(),
+        online=True
+    ).save()
     return JSONResponse(
         content="Data accepted",
         status_code=status.HTTP_202_ACCEPTED
     )
 
 
-@router.get("/{host}/regions")
+@router.get("/{host}/players-data")
+async def get_players_data(
+        host: str = Query(..., regex=schemas.ip_validation_regex)
+) -> JSONResponse:
+    server_obj = await models.Server.get_or_none(host=host)
+    if server_obj is None:
+        raise exc.not_exist_exception
+    server_data_obj = await models.ServerData.get(server_obj.id)
+    return JSONResponse(
+        content=server_data_obj.players_data,
+        status_code=status.HTTP_200_OK
+    )
+
+
+@router.post("/{host}/region-image")
+async def upload_region_images(
+        regions: list[UploadFile] = File(...),
+        current_server: schemas.Server = Depends(get_current_server)
+) -> JSONResponse:
+    server_regions_folder = servers_folder.get_regions_path(current_server.id)
+    # bg_tasks.add_task(servers_folder.regions.process_mca_files, regions)
+    return JSONResponse(
+        content=f"Path: {server_regions_folder}/",
+        status_code=status.HTTP_200_OK
+    )
+
+
+@router.get("/{host}/region-image")
 async def get_region_image(
         world: str,
         region_x: int,
-        region_y: int,
-        host: str = Query(..., regex=schemas.ip_validation_regex),
+        region_z: int,
+        host: str = Query(..., regex=schemas.ip_validation_regex)
 ) -> FileResponse:
     server_obj = await models.Server.get_or_none(host=host)
     if server_obj is None:
         raise exc.not_exist_exception
-    file_path = f"servers/regions/{server_obj.id}/{world}_r.{region_x}.{region_y}.png"
+    file_path = f"servers/regions/{server_obj.id}/{world}_r.{region_x}.{region_z}.mca.png"
     if not os.path.exists(file_path):
         raise exc.not_exist_exception
     return FileResponse(file_path)
-
-
-@router.post("/{host}/upload-region-image")
-async def upload_region_image(
-        world: str,
-        region_x: int,
-        region_z: int,
-        image: UploadFile = File(...),
-        current_server: schemas.Server = Depends(authorize_server)
-) -> JSONResponse:
-    file_path = f"servers/regions/{current_server.id}/{world}_r.{region_x}.{region_z}.png"
-    server_snapshot.save_image_file(file_path, image)
-    return JSONResponse(
-        content=file_path,
-        status_code=status.HTTP_202_ACCEPTED
-    )
