4d7f348e5acc0313fd275a1cef92565ae60a84be
Bogger-byte <nik.troshnev@gmail.com>
2022-04-01 22:46:18 +0700

    Created exceptions module for auth

---
+++ app/controllers/authentication/exceptions.py
@@ -0,0 +1,12 @@
+__all__ = [
+    "credentials_exception"
+]
+
+from fastapi import HTTPException, status
+
+
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
