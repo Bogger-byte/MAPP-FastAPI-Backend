fdb0558c6c9b06d0b40594a27094b94e02c8b01e
Bogger-byte <nik.troshnev@gmail.com>
2022-04-01 22:51:11 +0700

    Removed admin dependency. Now it is based on permissions system

--- app/controllers/authentication/users.py
+++ app/controllers/authentication/users.py
@@ -1,48 +1,45 @@
 __all__ = [
     "authorize_user",
     "get_current_user",
-    "get_current_admin"
 ]
 
-from fastapi import Depends
+from fastapi import Depends, Request, HTTPException, status
 
 from app.controllers.security import verify_password, JWTToken
-from app import exceptions as exc
 from app import schemas
 from app import models
 
 from .jwt import require_jwt_access_token
+from .exceptions import credentials_exception
 
 
 async def authorize_user(
         username: str,
         password: str
-) -> schemas.User:
+) -> models.User:
     user_obj = await models.User.get_or_none(username=username)
     if user_obj is None:
-        raise exc.credentials_exception
+        raise credentials_exception
     if not verify_password(password, user_obj.password):
-        raise exc.credentials_exception
-    return await schemas.User.from_tortoise_orm(user_obj)
+        raise credentials_exception
+    return user_obj
 
 
 async def get_current_user(
+        request: Request,
         token: JWTToken = Depends(require_jwt_access_token)
 ) -> schemas.User:
-    if token.is_expired:
-        raise exc.credentials_exception
     user_obj = await models.User.get_or_none(id=token.subject)
     if user_obj is None:
-        raise exc.credentials_exception
+        raise credentials_exception
     if user_obj.is_disabled:
-        raise exc.deleted_exception
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User was disabled"
+        )
+    if not user_obj.permissions.is_permitted_to(request.method, request.url.path):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Permission denied"
+        )
     return await schemas.User.from_tortoise_orm(user_obj)
-
-
-async def get_current_admin(
-        token: JWTToken = Depends(require_jwt_access_token)
-) -> schemas.User:
-    user = await get_current_user(token)
-    if not user.is_super:
-        raise exc.forbidden_act_exception
-    return user
