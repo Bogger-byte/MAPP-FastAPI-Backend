f80bde8e441cd1d6d35ce354cba916625fdf9cdb
Bogger-byte <nik.troshnev@gmail.com>
2022-04-01 22:55:19 +0700

    Migrated to stronger secured jwt lib

--- app/controllers/security.py
+++ app/controllers/security.py
@@ -1,13 +1,18 @@
 __all__ = ["verify_password", "get_password_hash", "JWTToken"]
 
+import datetime
+from datetime import timedelta
 from typing import Optional, Type
 
-from jose import jwt, JWTError
+import jwt
 from passlib.context import CryptContext
-from datetime import datetime, timedelta
 
-from app.settings import JWT_SECRET, JWT_DECODE_ALGORYTHM, JWT_ACCESS_TOKEN_EXPIRE
-
+from app.settings import (
+    JWT_SECRET,
+    JWT_DECODE_ALGORYTHM,
+    JWT_ACCESS_TOKEN_EXPIRE,
+    JWT_REFRESH_TOKEN_EXPIRE
+)
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -24,55 +29,56 @@ def get_password_hash(password: str) -> str:
 
 
 class JWTToken:
-    def __init__(self, token: str) -> None:
-        try:
-            self._data = jwt.decode(token, JWT_SECRET, algorithms=[JWT_DECODE_ALGORYTHM])
-        except JWTError:
-            raise ValueError(f"Can't decode JWT token: {token}")
-
-        self._type = self._data.get("type")
-        self._expire = self._data.get("exp")
-        self.subject = self._data.get("sub")
-
-        if self._type is None or self.subject is None or self._expire is None:
-            raise ValueError(f"Missing required fields in JWT token: {self._data} -> [type, sub, exp, sp]")
-
-    @property
-    def is_expired(self) -> bool:
-        return datetime.utcfromtimestamp(self._expire) < datetime.utcnow()
+    def __init__(
+            self,
+            _type: str,
+            subject: str,
+            issuer: str,
+            expire_delta: Optional[timedelta] = None,
+    ) -> None:
+        self.type = _type
+        self.subject = subject
+        self.issuer = issuer
+        if expire_delta is None:
+            if _type == "access":
+                expire_delta = JWT_ACCESS_TOKEN_EXPIRE
+            if _type == "refresh":
+                expire_delta = JWT_REFRESH_TOKEN_EXPIRE
+        self.expire_delta = expire_delta
 
     @property
     def is_refresh_token(self) -> bool:
-        return self._type == "refresh"
+        return self.type == "refresh"
 
     @property
     def is_access_token(self) -> bool:
-        return self._type == "access"
+        return self.type == "access"
 
     @classmethod
-    def create_access_token(
-            cls: Type["JWTToken"],
-            subject: str,
-            expires_delta: Optional[timedelta] = JWT_ACCESS_TOKEN_EXPIRE
-    ) -> str:
-        expire_time = datetime.utcnow() + expires_delta
-        data = {
-            "type": "access",
-            "sub": subject,
-            "exp": expire_time
-        }
-        return jwt.encode(data, JWT_SECRET, algorithm=JWT_DECODE_ALGORYTHM)
+    def decode(cls: Type["JWTToken"], token: str) -> "JWTToken":
+        try:
+            data = jwt.decode(
+                token,
+                JWT_SECRET,
+                leeway=JWT_ACCESS_TOKEN_EXPIRE,
+                algorithms=[JWT_DECODE_ALGORYTHM],
+                options={"require": ["exp", "sub", "iss", "type"]}
+            )
+        except jwt.DecodeError or jwt.MissingRequiredClaimError:
+            raise ValueError(f"Can't decode JWT token: {token}")
+        except jwt.ExpiredSignatureError:
+            raise ValueError(f"Token is expired")
 
-    @classmethod
-    def create_refresh_token(
-            cls: Type["JWTToken"],
-            subject: str,
-            expires_delta: Optional[timedelta] = JWT_ACCESS_TOKEN_EXPIRE
-    ) -> str:
-        expire_time = datetime.utcnow() + expires_delta
+        _type = data.get("type")
+        subject = data.get("sub")
+        issuer = data.get("iss")
+        return cls(_type, subject, issuer)
+
+    def encode(self) -> str:
         data = {
-            "type": "refresh",
-            "sub": subject,
-            "exp": expire_time
+            "type": self.type,
+            "sub": self.subject,
+            "iss": self.issuer,
+            "exp": datetime.datetime.utcnow() + self.expire_delta
         }
         return jwt.encode(data, JWT_SECRET, algorithm=JWT_DECODE_ALGORYTHM)
