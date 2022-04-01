eb07c571477e590d344489c1b93ad8cd9f022ba0
Bogger-byte <nik.troshnev@gmail.com>
2022-04-01 22:49:35 +0700

    Created detailed exceptions

--- app/controllers/authentication/xbox.py
+++ app/controllers/authentication/xbox.py
@@ -1,9 +1,12 @@
+__all__ = [
+    "require_xbox_account"
+]
 
-
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 
 from app import schemas
 from app import models
+from app import exceptions as exc
 
 from .users import get_current_user
 
@@ -12,8 +15,11 @@ async def require_xbox_account(
         current_user: schemas.User = Depends(get_current_user)
 ) -> schemas.Xbox:
     user_obj = await models.User.get_or_none(id=current_user.id)
-    await user_obj.fetch_related("linked_xbox_accounts")
-    xbox_accounts: list[schemas.Xbox] = user_obj.__getattribute__("linked_xbox_accounts")
+    await user_obj.fetch_related("xbox_accounts")
+    xbox_accounts: list[schemas.Xbox] = user_obj.__getattribute__("xbox_accounts")
     if not xbox_accounts:
-        raise exc.not_exist_exception
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Xbox accounts was not located"
+        )
     return xbox_accounts[0]
